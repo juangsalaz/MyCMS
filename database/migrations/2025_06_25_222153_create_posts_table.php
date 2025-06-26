07e5919c8c4ac7078013876610b71e929bc0410b
Juang Salaz Prabowo <juang.salaz.prabowo@gmail.com>
2025-06-26 07:08:29 +0700

    Creating migration table

--- database/migrations/2025_06_25_222153_create_posts_table.php
+++ database/migrations/2025_06_25_222153_create_posts_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('excerpt')->nullable();
+            $table->longText('content');
+            $table->string('image')->nullable();
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->timestamp('published_at')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/migrations/2025_06_25_222207_create_pages_table.php
+++ database/migrations/2025_06_25_222207_create_pages_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('body');
+            $table->enum('status', ['draft', 'published'])->default('draft');
             $table->timestamps();
         });
     }
